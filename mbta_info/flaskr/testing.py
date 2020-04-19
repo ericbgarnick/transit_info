d9abac525552461f344c2be6adc18b8fadcc1aaf
Eric Garnick <eric.b.garnick@gmail.com>
2020-04-19 19:06:10 -0400

    Remove unused files

--- mbta_info/flaskr/testing.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import schemas
-
-file_name = 'lines.csv'
-model_name = os.path.splitext(file_name)[0].rstrip('s')  # data file name may be pluralized
-model_schema = getattr(schemas, model_name.title() + 'Schema')
-print("MODEL SCHEMA", model_schema)
