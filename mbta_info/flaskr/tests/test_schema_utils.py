154ffa2954b58ad9c4dc15b5ebc18135894d4af8
Eric Garnick <eric.b.garnick@gmail.com>
2020-04-25 19:37:40 -0400

    Add tests for numbered_type_enum_key

--- mbta_info/flaskr/tests/test_schema_utils.py
+++ mbta_info/flaskr/tests/test_schema_utils.py
@@ -1,11 +1,35 @@
+from typing import Optional
+
 import pytest
 
-from mbta_info.flaskr.schema_utils import timezone_enum_key
+from mbta_info.flaskr.schema_utils import timezone_enum_key, numbered_type_enum_key
 
 
 @pytest.mark.parametrize(
     "raw_value,updated_value",
     [(None, None), ("", ""), ("a", "a"), ("/", "_"), ("a/a", "a_a")],
 )
-def test_timezone_enum_key(raw_value, updated_value):
+def test_timezone_enum_key(raw_value: Optional[str], updated_value: Optional[str]):
     assert timezone_enum_key(raw_value) == updated_value
+
+
+@pytest.mark.parametrize("raw_value", ("a", "1.1", "-1"))
+def test_numbered_type_enum_key_bad_value(raw_value):
+    with pytest.raises(TypeError):
+        numbered_type_enum_key(raw_value)
+
+
+@pytest.mark.parametrize(
+    "raw_value,updated_value",
+    [(None, None), ("", None), ("0", "type_0"), ("234", "type_234")],
+)
+def test_numbered_type_enum_key_no_default(raw_value: Optional[str], updated_value: Optional[str]):
+    assert numbered_type_enum_key(raw_value) == updated_value
+
+
+@pytest.mark.parametrize(
+    "raw_value,updated_value",
+    [(None, "type_0"), ("", "type_0"), ("0", "type_0"), ("234", "type_234")],
+)
+def test_numbered_type_enum_key_with_default(raw_value: Optional[str], updated_value: str):
+    assert numbered_type_enum_key(raw_value, default_0=True) == updated_value
