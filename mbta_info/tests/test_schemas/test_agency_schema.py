19e53ef6407bb26d52e8cbeb4881e7f438bbf32e
Eric Garnick <eric.b.garnick@gmail.com>
2020-05-08 08:19:43 -0400

    Test AgencySchema load bad data

--- mbta_info/tests/test_schemas/test_agency_schema.py
+++ mbta_info/tests/test_schemas/test_agency_schema.py
@@ -1,7 +1,10 @@
+import marshmallow as mm
+import pytest
+
 from mbta_info.flaskr import schemas, models as mbta_models
 
 
-def test_validate_good_data():
+def test_load_good_data():
     # GIVEN
     agency_data = {
         "agency_id": 1,
@@ -23,3 +26,31 @@ def test_validate_good_data():
         elif key == "agency_lang":
             value = getattr(mbta_models.LangCode, value.lower())
         assert getattr(agency_obj, key) == value
+
+
+@pytest.mark.parametrize(
+    "agency_data_update",
+    (
+        {"agency_id": "a"},
+        {"agency_name": None},
+        {"agency_url": "bad_url"},
+        {"agency_timezone": "bad_timezone"},
+        {"agency_lang": "bad_lang"},
+        {"bad_key": "anything"},
+    ),
+)
+def test_load_bad_data(agency_data_update):
+    # GIVEN
+    agency_data = {
+        "agency_id": 1,
+        "agency_name": "Agency",
+        "agency_url": "http://www.agency.com",
+        "agency_timezone": "America/New_York",
+        "agency_lang": "EN",
+        "agency_phone": "555-555-5555",
+    }
+    agency_data.update(agency_data_update)
+
+    # THEN
+    with pytest.raises(mm.ValidationError):
+        schemas.AgencySchema().load(agency_data)
